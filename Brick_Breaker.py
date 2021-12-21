1d981c36aa960f7e6763aba4e8a2f269100fd5b3
Joe Ellul-Turner <joeet@pop-os.localdomain>
2021-12-21 15:00:10 +0000

    added winning text + game reset

--- Brick_Breaker.py
+++ Brick_Breaker.py
@@ -9,6 +9,9 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Breakout')
 
+# define font
+font = pygame.font.SysFont('Constantia', 30)
+
 # define colours
 bg = (234, 218, 184)
 # block colours
@@ -18,12 +21,22 @@ block_blue = (69, 177, 232)
 # paddle colours
 paddle_col = (142, 135, 123)
 paddle_outline = (100, 100, 100)
+# text colour
+text_col = (78, 81, 139)
 
 # define game variables
 cols = 6
 rows = 6
 clock = pygame.time.Clock()
 fps = 60
+live_ball = False
+game_over = 0
+
+
+# function for outputting text onto the screen
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
 
 
 # brick wall class
@@ -76,14 +89,7 @@ class wall():
 # paddle class
 class paddle():
     def __init__(self):
-        # define paddle variables
-        self.height = 20
-        self.width = int(screen_width / cols)
-        self.x = int((screen_width / 2) - (self.width / 2))
-        self.y = screen_height - (self.height * 2)
-        self.speed = 10
-        self.rect = Rect(self.x, self.y, self.width, self.height)
-        self.direction = 0
+        self.reset()
 
     def move(self):
         # reset movement direction
@@ -100,19 +106,63 @@ class paddle():
         pygame.draw.rect(screen, paddle_col, self.rect)
         pygame.draw.rect(screen, paddle_outline, self.rect, 3)
 
+    def reset(self):
+        # define paddle variables
+        self.height = 20
+        self.width = int(screen_width / cols)
+        self.x = int((screen_width / 2) - (self.width / 2))
+        self.y = screen_height - (self.height * 2)
+        self.speed = 10
+        self.rect = Rect(self.x, self.y, self.width, self.height)
+        self.direction = 0
+
 
 # ball class
 class game_ball():
     def __init__(self, x, y):
-        self.ball_rad = 10
-        self.x = x - self.ball_rad
-        self.y = y
-        self.rect = Rect(self.x, self.y, self.ball_rad * 2, self.ball_rad * 2)
-        self.speed_x = 4
-        self.speed_y = -4
-        self.game_over = 0
+        self.reset(x, y)
 
     def move(self):
+
+        # collision threshold
+        collision_thresh = 5
+
+        # start off with the assumption that the wall has been destroyed completely
+        wall_destroyed = 1
+        row_count = 0
+        for row in wall.blocks:
+            item_count = 0
+            for item in row:
+                # check collision
+                if self.rect.colliderect(item[0]):
+                    # check if collision was from above
+                    if abs(self.rect.bottom - item[0].top) < collision_thresh and self.speed_y > 0:
+                        self.speed_y *= -1
+                    # check if collision was from below
+                    if abs(self.rect.top - item[0].bottom) < collision_thresh and self.speed_y < 0:
+                        self.speed_y *= -1
+                        # check if collision was from left
+                    if abs(self.rect.right - item[0].left) < collision_thresh and self.speed_x > 0:
+                        self.speed_x *= -1
+                    # check if collision was from right
+                    if abs(self.rect.left - item[0].right) < collision_thresh and self.speed_x < 0:
+                        self.speed_x *= -1
+                    # reduce the block's strength by doing damage to it
+                    if wall.blocks[row_count][item_count][1] > 1:
+                        wall.blocks[row_count][item_count][1] -= 1
+                    else:
+                        wall.blocks[row_count][item_count][0] = (0, 0, 0, 0)
+
+                # check if block still exists, in whcih case the wall is not destroyed
+                if wall.blocks[row_count][item_count][0] != (0, 0, 0, 0):
+                    wall_destroyed = 0
+                # increase item counter
+                item_count += 1
+            # increase row counter
+            row_count += 1
+        # after iterating through all the blocks, check if the wall is destroyed
+        if wall_destroyed == 1:
+            self.game_over = 1
 
         # check for collision with walls
         if self.rect.left < 0 or self.rect.right > screen_width:
@@ -124,6 +174,19 @@ class game_ball():
         if self.rect.bottom > screen_height:
             self.game_over = -1
 
+        # look for collission with paddle
+        if self.rect.colliderect(player_paddle):
+            # check if colliding from the top
+            if abs(self.rect.bottom - player_paddle.rect.top) < collision_thresh and self.speed_y > 0:
+                self.speed_y *= -1
+                self.speed_x += player_paddle.direction
+                if self.speed_x > self.speed_max:
+                    self.speed_x = self.speed_max
+                elif self.speed_x < 0 and self.speed_x < -self.speed_max:
+                    self.speed_x = -self.speed_max
+            else:
+                self.speed_x *= -1
+
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
 
@@ -134,6 +197,16 @@ class game_ball():
                            self.ball_rad)
         pygame.draw.circle(screen, paddle_outline, (self.rect.x + self.ball_rad, self.rect.y + self.ball_rad),
                            self.ball_rad, 3)
+
+    def reset(self, x, y):
+        self.ball_rad = 10
+        self.x = x - self.ball_rad
+        self.y = y
+        self.rect = Rect(self.x, self.y, self.ball_rad * 2, self.ball_rad * 2)
+        self.speed_x = 4
+        self.speed_y = -4
+        self.speed_max = 5
+        self.game_over = 0
 
 
 # create a wall
@@ -153,20 +226,39 @@ while run:
 
     screen.fill(bg)
 
-    # draw wall
+    # draw all objects
     wall.draw_wall()
-
-    # draw paddle
     player_paddle.draw()
-    player_paddle.move()
-
-    # draw ball
     ball.draw()
-    ball.move()
+
+    if live_ball:
+        # draw paddle
+        player_paddle.move()
+        # draw ball
+        game_over = ball.move()
+        if game_over != 0:
+            live_ball = False
+
+    # game outcomes
+    if not live_ball:
+        if game_over == 0:
+            live_ball = True
+            ball.reset(player_paddle.x + (player_paddle.width // 2), player_paddle.y - player_paddle.height)
+            player_paddle.reset()
+            wall.create_wall()
+        elif game_over == 1:
+            draw_text('YOU WON!', font, text_col, 240, screen_height // 2 + 50)
+            draw_text('CLICK ANYWHERE TO START', font, text_col, 100, screen_height // 2 + 100)
+        elif game_over == -1:
+            live_ball = True
+            ball.reset(player_paddle.x + (player_paddle.width // 2), player_paddle.y - player_paddle.height)
+            player_paddle.reset()
+            wall.create_wall()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
 
     pygame.display.update()
 
